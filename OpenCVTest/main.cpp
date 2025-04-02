1d5c942953dd5dd2febd91ec96432af1cf1f3f46
yeduk3 <yeduk33@gmail.com>
2025-04-02 13:27:35 +0900

    Initial Commit

---
+++ OpenCVTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenCVTest
+//
+//  Created by 이용규 on 4/2/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
